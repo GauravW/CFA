c46b9538daf67bb01696385a6c230d68f8f72331
GauravW <gauravwaratkar24@gmail.com>
2021-08-04 18:23:21 +0530

    Added Good plot of velocity

--- velocity.py
+++ velocity.py
@@ -1,14 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 def velocity(v0, a, t):
     return (v0 + a*t)
-
 
 
 def main():
     v0 = 0
     t = np.arange(0,100,1)
-    a = 10
-    velocity = (v0, a, t)
+    a = -10
+    vel = velocity(v0, a, t)
 
+    plt.plot(t, vel)
+    plt.title('Good/Nice/Perfect/Beautifool Plot')
+    plt.xlabel('Time')
+    plt.ylabel('Velocity')
+    plt.savefig('GoodPlot.pdf')
 
 
 if __name__ == "__main__":
