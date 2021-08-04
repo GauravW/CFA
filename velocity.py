ab2b3f95b32b5183ee6066860908d5c50cd168bc
harshkumar13 <harshkosli13@gmail.com>
2021-08-04 19:09:22 +0530

    Added plot function for diplacement.

--- velocity.py
+++ velocity.py
@@ -6,9 +6,17 @@ def velocity(v0, a, t):
     return (v0 + a*t)
 
 
+def plot_dis(x,y, title, xlabel, ylabel, plot_name):
+    plt.plot(x, y)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.savefig(plot_name)
+
+
 def main():
-    v0 = 0
-    t = np.arange(0,200,1)
+    v0 = 10
+    t = np.arange(0,2,0.1)
     a = -10
     vel = velocity(v0, a, t)
 
